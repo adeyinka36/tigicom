8b3bd802682a8a9f54a20c223330d0b08aa2a776
Adeyinka Giwa <adeyinka36@gmail.com>
2020-05-28 21:39:55 +0100

    fixed sme typos in about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,9 +7,9 @@ const About =()=>{
         <div className="about_container">
           <div className="about_top">
             <div className="about_text">
-               <h1>WHO AM I?</h1>
-               <p> Tigi is a  20 years old, who specializes in natural lace wigs and has mastered making wigs on the sewing machine. She is known for her flawless customizing of the lace hairline and her signature curls.</p>
-               <p> Tigi is also has a passion for eyelashes .This led her  to become one of the most reliable and afoordable merhants of eyelashes in london</p>
+               <h1>WHO IS TIGI?</h1>
+               <p> Tigi is a  20 year old, who specializes in natural lace wigs and has mastered making wigs on the sewing machine. She is known for her flawless customizing of the lace hairline and her signature curls.</p>
+               <p> Tigi also has a passion for eyelashes .This led her  to become one of the most reliable and afordable merchants of eyelashes in london</p>
             </div>
             <div className="about_img">
                <img src={wigs} alt="prtty girl"></img>
@@ -18,7 +18,7 @@ const About =()=>{
           <div className="about_middle">
             <div className="about_text">
                <h1>WHAT I DO</h1>
-               <p> In a nutshell, if you need to buy quality eweaves, wigs or eyelashes, you have come to the perfect place. Here is a list of some of my services:</p>
+               <p> In a nutshell, if you need to buy quality weaves, wigs or eyelashes, you have come to the perfect place. Here is a list of some of my services:</p>
                <h2>Wig Installation</h2>
                <h2>Eyelashes Installation</h2>
                <h2>Sale of Wigs and Eyelashes</h2>
